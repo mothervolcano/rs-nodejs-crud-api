a361898b7cc8c8481b9654adc399fd939bf1c180
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-17 10:32:58 +0100

    init: install packages; set up server

---
+++ index.js
@@ -0,0 +1,12 @@
+import http from 'http';
+
+const PORT = 3001;
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain'});
+	res.end('We are on!');
+});
+
+server.listen(PORT);
+
+console.log(`Server running on PORT ${PORT}`)
